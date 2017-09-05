458990d03af35eb094e968211f8faa5e3e415748
Jonas Pollnow <jpjonas35@gmail.com>
2017-09-05 13:34:33 -0300

    Practica 2
    Adiconados comentarios

--- Práctica 2/Ejercicio 6.py
+++ Práctica 2/Ejercicio 6.py
@@ -1,7 +1,26 @@
-def main(c,x,n):
+def inversiones(c,x,n):
+    """
+    Representación de los datos:
+    Representamos inversiones con números
+
+    Signatura:
+    contaNumeros: float -> float ->  int -> void
+    El primer parámetro representa el capital inicial, el segundo la tasa de interés,
+    y el tercer el número de años a calcular.
+    Declaracion de propósito:
+    La función calcula el monto final a otener.
+
+    Ejemplos:
+    >>> inversiones(10,90,10) = 6131,06
+    >>> inversiones(100,70,5) = 1419,85
+    >>> inversiones(1500,50,15) = 656840,83
+    """
     t=1
     for y in range(0,n):
         t=t*(1+(x/100))
     print(c*t)
 
-main(10,90,10)
+#Testes
+inversiones(10,90,10)
+inversiones(100,70,5)
+inversiones(1500,50,15)
--- Práctica 2/Ejercicio 7.py
+++ Práctica 2/Ejercicio 7.py
@@ -1,4 +1,20 @@
 def numTriangulares(n):
+    """
+    Representación de los datos:
+    Representamos números triangulares con enteros
+
+    Signatura:
+    contaNumeros: int -> void
+    El parámetro es la cantidad de números triangulares
+
+    Declaracion de propósito:
+    La función imprime la cantidad de números triangulares deseados.
+
+    Ejemplos:
+    >>> contaNumeros(2,5)={2,3,4,5}
+    >>> contaNumeros(10,12)={10,11,12}
+    >>> contaNumeros(-10,-5)={-10,-9,-8,-7,-6,-5}
+    """
     t=0
     for x in range(1,n+1):
         t=t+x
