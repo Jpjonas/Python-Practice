030d123763810515af480ef04d038cd76287a02a
Jonas Pollnow <jpjonas35@gmail.com>
2017-09-09 18:20:40 -0300

    Trabajo Practico
    Desarollo del menu y comienzo de la estructura de palabras

--- Trabajo Práctico 1/Tutti Frutti.py
+++ Trabajo Práctico 1/Tutti Frutti.py
@@ -1,12 +1,43 @@
-#bibliotecas
+#Bibliotecas
 import random
 import string
 
+def menu():
+    n=1 # Número de jugadores
+    o=0
+    while(o!=3)
+        print("[1] Jugadores")
+        print("[2] Jugar")
+        print("[3] Salir")
+        o=int(input("Selecione una opción: "))
+        if(o==1):
+            n=verificaJugadores()
+        if(0==2):
+            jugar(n)
+
+def verificaJugadores():
+    #verificar si é um número y si esta dentro do possivel
+    return int(input("Ingrese la cantidad de jugadores[1,2,3,4,5]: "))
 
 
-def main():
-    # Número de jugadores
-    n=int(input("Ingrese la cantidad de jugadores[2,3,4,5]: "))
+#n->Número de jugadores
+def jugar(n):
     # Letra generada al azar
     l=random.choice(string.ascii_letters)
-    for(j in range(1,n)):
+    print("La letra es: ",l)
+
+    matriz = [] # lista vacia
+	for i in range(0,n):
+        # cria a linea i
+        linha = [] # lista vacia
+	        for j in range(0,7):
+
+            linha.append(input(""))
+
+	        matriz.append(linha)
+
+
+
+
+
+menu()
