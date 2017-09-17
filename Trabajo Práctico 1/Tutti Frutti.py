9a6d9096ac52810266920068c8b96f051f597d41
Jonas Pollnow <jpjonas35@gmail.com>
2017-09-17 09:54:35 -0300

    Trabajo Practico 1

--- Trabajo Práctico 1/Tutti Frutti.py
+++ Trabajo Práctico 1/Tutti Frutti.py
@@ -2,7 +2,6 @@
 import random
 import string
 import os #executar somente en linux
-import numpy #biblioteca manipulacion de matrices
 #instalar biblioteca pytest
 
 #Finalizada
@@ -11,12 +10,16 @@ def menu():
     opcion = 0
     while(opcion!=3):
         os.system('clear')
-        print '\nTUTTI FRUTTI'
-        print '\n',numJugadores,' Jugadores'
-        print '\n[1] Cambiar Cantidad de Jugadores'
-        print '[2] Jugar'
-        print '[3] Salir'
-        opcion=int(input("Selecione una opcion: "))
+        print('\nTUTTI FRUTTI')
+        print('\n',numJugadores,' Jugadores')
+        print('\n[1] Cambiar Cantidad de Jugadores')
+        print('[2] Jugar')
+        print('[3] Salir')
+        try:
+            opcion=int(input("Selecione una opcion: "))
+        except:
+            menu()
+            pass
         if(opcion==1):
             numJugadores = cambiaNumeroJugadores()
         if(opcion==2):
@@ -26,36 +29,82 @@ def menu():
 
 
 def jugar(numJugadores):
-    listaPuntos=[]
-    listaLetras=[]
-    for jugadores in range(0,numJugadores):
-        listaPuntos.append(0)
+    listaPuntos = [0] * numJugadores
+    print(listaPuntos)
+    listaLetras = []
 
-    while(max(listaPuntos)<200):
-        letra = eligeLetra(listaPuntos)
+    if(0==0):
+    #while(max(listaPuntos)<200):
+        os.system('clear')
+
+        #Elige la letra para la rodada
+        letra = eligeLetra(listaLetras)
         listaLetras.append(letra)
 
-        matrizPalavras=[]
-        for jugadores in range(0,numJugadores+1):
-            print '\nJUGADOR ',jugadores
-            linea=[]
-            for numCategoria in range(0,7):
-                linea.append(recibePalabra(numCategoria))
-            matrizPalavras.append(linea)
+        #Popula los campos de la matriz de la rodada
+        matrizPalabras = populaMatriz(numJugadores)
 
-        contaPuntos(matrizPalavras)
-        #print(matrizPalavras)
+        #Imprime la matriz de la rodada
+        imprimeMatriz(matrizPalabras,numJugadores)
+
+        #Conta los puntos de cada jugador
+        listaPuntos = contarPuntos(matrizPalabras,numJugadores)
+
+    #Comando para el ganador:
+    #...
+
+
+def contarPuntos(matrizPalabras,numJugadores):#contagen de los puntos
+    listaPuntos=[]
+    #Contagen de los puntos de cada jugadores
+    #Sumando los puntos obtenidos antes
+    for jugador in range(0,numJugadores):
+        for numCategoria in range(7):
+            for jugadorComp in range(0,numJugadores):
+                if(matrizPalabras[numCategoria][jugador] == matrizPalabras[numCategoria][jugadorComp]):
+                    print("a")
+
+    return listaPuntos
+
+
+#Finalizada (cambiar hola)
+def populaMatriz(numJugadores):
+    #Crea la matriz de jugadores X categorias para cada rodada
+    matrizPalabras = [['' for i in range(0,numJugadores)] for j in range(0,7)]
+
+    #Pregunta las palabras para cada jugador
+    for jugador in range(0,numJugadores):
+        #Imprime cual es el jugador
+        print('\nJUGADOR ',jugador+1)
+        for numCategoria in range(0,7):
+            #Recibe la palabra del jugador
+            #matrizPalabras[numCategoria][jugador] = recibePalabra(numCategoria)
+            matrizPalabras[numCategoria][jugador] = 'hola'
+    return matrizPalabras
+
+#Finalizada
+def imprimeMatriz(matrizPalabras,numJugadores):
+    #Imprime la matriz
+    #os.system('clear')
+    for jugador in range(0,numJugadores):
+        #Imprime cual es el jugador
+        print('\nJUGADOR ',jugador+1)
+        for numCategoria in range(0,7):
+            #Recibe la palabra del jugador
+            print('\n',matrizPalabras[numCategoria][jugador])
+
+    input('Enter pra continuar')
 
 #Finalizada
 def cambiaNumeroJugadores():
     numJugadores=2
     try:
-        numJugadores = int(input('\nIngrese la cantidad de jugadores[1,2,3,4,5]: '))
-        if(numJugadores<1)or(numJugadores>5):
-            print '\nCantidad invalida!'
+        numJugadores = int(input('\nIngrese la cantidad de jugadores[2,3,4,5]: '))
+        if(numJugadores<2)or(numJugadores>5):
+            print('\nCantidad invalida!')
             return cambiaNumeroJugadores()
     except:
-        print '\nCantidad invalida!'
+        print('\nCantidad invalida!')
         return cambiaNumeroJugadores()
         pass
     return numJugadores
@@ -68,7 +117,7 @@ def eligeLetra(listaLetrasAnteriores):
         if(letraAnterior == nuevaLetra):
             repetida = True
     if(repetida == False):
-        print '\nLa letra es: ',nuevaLetra
+        print('\nLa letra es: ',nuevaLetra)
         return nuevaLetra
 
 #Finalizada
@@ -87,20 +136,8 @@ def recibePalabra(numCategoria):
         txtCategoria = 'Ingrese una fruta: '
     if(numCategoria==6):
         txtCategoria = 'Ingrese un pais: '
-    return str(input(txtCategoria))
+    return input(txtCategoria)
 
-'''
-def contaPuntos(matrizPalavras):#contagen de los puntos
-    listaPuntos=[]
-    #Contagen de los puntos de cada jugadores
-    #Sumando los puntos obtenidos antes
-    for x in range(matrizPalavras):
-        for y in range(x):
-            for i in range(matrizPalavras):
-                for j in range(i):
-                    #if(matrizPalavras[][]==matrizPalavras[][])
-                    print("a")
-    return listaPuntos
-'''
+
 
 menu()#llamada del juego en el terminal
